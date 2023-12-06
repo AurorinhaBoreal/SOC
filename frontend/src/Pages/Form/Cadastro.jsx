53800fd3587146bc265591a1be025171d2f059ca
Arthur_Tu44 <88666036+ArthurTu44@users.noreply.github.com>
2023-12-06 10:21:48 -0300

    FrontEnd: Log in form update

--- frontend/src/Pages/Form/Cadastro.jsx
+++ frontend/src/Pages/Form/Cadastro.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import '../../css/styles.css'
+import logo from '../../Assets/logo branca.png'
 import API from '../../API'
-import NavForm from '../../Components/NavForm/NavForm'
 
 import { MdEmail } from 'react-icons/md'
 import { PiBarcodeBold } from 'react-icons/pi'
@@ -15,15 +15,15 @@ import { Link } from 'react-router-dom'
 const Cadastro = () => {
 
   const [mostrarSenha, setMostrarSenha] = useState("password")
-  const [senhaView, setSenhaView] = useState(<GrFormViewHide className='icones'/>)
+  const [senhaView, setSenhaView] = useState(<GrFormViewHide className='form__items__icones'/>)
 
   function verSenha(){
     setMostrarSenha("text")
-    setSenhaView(<GrFormView className='icones'/>)
+    setSenhaView(<GrFormView className='form__items__icones'/>)
 
     if(mostrarSenha == "text"){
       setMostrarSenha("password")
-      setSenhaView(<GrFormViewHide className='icones'/>)
+      setSenhaView(<GrFormViewHide className='form__items__icones'/>)
     }
   }
 
@@ -62,57 +62,69 @@ const Cadastro = () => {
 
   return (
     <div className='cadastro'>
-        <NavForm/>
-        <div className='form flex'>
-          <h1 className='form__title'>Cadastro</h1>
-          <form className='flex' 
-          onSubmit={handleRegister}>
-            <div className='form__items emailInput'>
-              <MdEmail className='icones'/>
-              <div className="form__items__input">
-                  <input type="text" name="" id="" required
-                  value={email} onChange={(e)=> setEmail(e.target.value)}/>
-                  <p className='form__items__placeholder'>Email</p>
-              </div>
-            </div>
-            <div className='form__div'>
-              <div className='form__items codInput'>
-                <PiBarcodeBold className='icones'/>
-                <div className="form__items__input">
-                  <input type="text" name="" id="" required
-                  value={codEtec} onChange={(e)=> setCodEtec(e.target.value)}/>
-                  <p className='form__items__placeholder'>Cód. ETEC</p>
+        <div className='form'>
+          <div className="form__esquerdo">
+            <div className='form__form'>
+              <h1 className='form__title'>Cadastro</h1>
+              <form className='flex' 
+              onSubmit={handleRegister}>
+                <div className='form__items emailInput'>
+                  <MdEmail className='form__items__icones'/>
+                  <div className="form__items__input">
+                      <input type="text" name="" id="" required
+                      value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                      <p className='form__items__placeholder'>Email</p>
+                  </div>
                 </div>
-              </div>
-              <div className='form__items rmInput'>
-                <BiSolidUser className='icones'/>
-                <div className="form__items__input">
-                  <input type="text" name="" id="" required
-                  value={login} onChange={(e)=> setLogin(e.target.value)}/>
-                  <p className='form__items__placeholder'>Nome Usuário</p>
-                </div>             
-              </div>
+                <div className='form__div'>
+                  <div className='form__items codInput'>
+                    <PiBarcodeBold className='form__items__icones'/>
+                    <div className="form__items__input">
+                      <input type="text" name="" id="" required
+                      value={codEtec} onChange={(e)=> setCodEtec(e.target.value)}/>
+                      <p className='form__items__placeholder'>Cód. ETEC</p>
+                    </div>
+                  </div>
+                  <div className='form__items rmInput'>
+                    <BiSolidUser className='form__items__icones'/>
+                    <div className="form__items__input">
+                      <input type="text" name="" id="" required
+                      value={login} onChange={(e)=> setLogin(e.target.value)}/>
+                      <p className='form__items__placeholder'>Nome Usuário</p>
+                    </div>             
+                  </div>
+                </div>
+                <div className="form__items senhaInput">
+                  <RiLockPasswordLine className='form__items__icones'/>
+                  <div className="form__items__input">
+                    <input type={mostrarSenha} name="" id="" required
+                    value={password} onChange={(e)=> setPassword(e.target.value)}/>
+                    <p className='form__items__placeholder'>Senha min. 8 caracteres</p>
+                  </div>
+                </div>
+                <div className="form__items repetirSenhaInput">
+                  <RiLockPasswordFill className='form__items__icones'/>
+                  <div className="form__items__input">
+                    <input type={mostrarSenha} name="" id="" required
+                    value={cPassword} onChange={(e)=> setCPassword(e.target.value)}/>
+                    <p className='form__items__placeholder'>Repetir Senha</p>
+                  </div>
+                  <button className='btnTransparente' onClick={verSenha} type="button">{senhaView}</button>
+                </div>
+                <Link className="form__link" to="/"><p>Já tem uma conta? Entre agora mesmo</p></Link>
+                <button className='form__btn__submit' type="submit">Cria conta</button>
+              </form>
             </div>
-            <div className="form__items senhaInput">
-              <RiLockPasswordLine className='icones'/>
-              <div className="form__items__input">
-                <input type={mostrarSenha} name="" id="" required
-                value={password} onChange={(e)=> setPassword(e.target.value)}/>
-                <p className='form__items__placeholder'>Senha min. 8 caracteres</p>
-              </div>
+          </div>
+          <div className="form__direito">
+            <header className='form__direito__header'>
+              <img src={logo}/>
+            </header>
+            <div className='form__direito__conteudo'>
+              <h1 className='form__direito__texto'>Seja Bem Vindo <br />
+              ao SOC</h1>
             </div>
-            <div className="form__items repetirSenhaInput">
-              <RiLockPasswordFill className='icones'/>
-              <div className="form__items__input">
-                <input type={mostrarSenha} name="" id="" required
-                value={cPassword} onChange={(e)=> setCPassword(e.target.value)}/>
-                <p className='form__items__placeholder'>Repetir Senha</p>
-              </div>
-              <button className='btnTransparente' onClick={verSenha} type="button">{senhaView}</button>
-            </div>
-            <Link className="form__link" to="/"><p>Já tem uma conta? Entre agora mesmo</p></Link>
-            <button className='form__btn__submit' type="submit">Cria conta</button>
-          </form>
+          </div>
         </div>
     </div>
   )
--- frontend/src/Pages/Form/Login.jsx
+++ frontend/src/Pages/Form/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import '../../css/styles.css'
-import NavForm from '../../Components/NavForm/NavForm'
+import logo from '../../Assets/logo branca.png'
 
 import { PiBarcodeBold } from 'react-icons/pi'
 import { BiSolidUser } from 'react-icons/bi'
@@ -14,62 +14,74 @@ import { FaGithub } from "react-icons/fa";
 const Login = () => {
 
   const [mostrarSenha, setMostrarSenha] = useState("password")
-  const [senhaView, setSenhaView] = useState(<GrFormViewHide className='icones'/>)
+  const [senhaView, setSenhaView] = useState(<GrFormViewHide className='form__items__icones'/>)
 
   function verSenha(){
     setMostrarSenha("text")
-    setSenhaView(<GrFormView className='icones'/>)
+    setSenhaView(<GrFormView className='form__items__icones'/>)
 
     if(mostrarSenha == "text"){
       setMostrarSenha("password")
-      setSenhaView(<GrFormViewHide className='icones'/>)
+      setSenhaView(<GrFormViewHide className='form__items__icones'/>)
     }
   }
 
   return (
     <div className='login'>
-        <NavForm/>
-        <div className='form flex'>
-          <h1 className='form__title'>login</h1>
-          <form className='flex'>
-            <div className='form__div'>
-              <div className='form__items input-cod'>
-                <PiBarcodeBold className='icones'/>
-                <div className='form__items__input'>
-                  <input type="text" name="" id="" required/>
-                  <p className='form__items__placeholder'>Cód. ETEC</p>
-                </div>             
-              </div>
-              <div className='form__items input-login'>
-                <BiSolidUser className='icones'/>
-                <div className='form__items__input'>
-                  <input type="text" name="" id="" required/>
-                  <p className='form__items__placeholder'>Nome Usuário</p>
+        <div className='form'>
+          <div className="form__esquerdo">
+            <div className='form__form'>
+              <h1 className='form__title'>login</h1>
+              <form className='flex'>
+                <div className='form__div'>
+                  <div className='form__items input-cod'>
+                    <PiBarcodeBold className='form__items__icones'/>
+                    <div className='form__items__input'>
+                      <input type="text" name="" id="" required/>
+                      <p className='form__items__placeholder'>Cód. ETEC</p>
+                    </div>             
+                  </div>
+                  <div className='form__items input-login'>
+                    <BiSolidUser className='form__items__icones'/>
+                    <div className='form__items__input'>
+                      <input type="text" name="" id="" required/>
+                      <p className='form__items__placeholder'>Nome Usuário</p>
+                    </div>
+                  </div>
+                </div>
+                <div className="form__items input-senha">
+                  <RiLockPasswordLine className='form__items__icones'/>
+                  <div className='form__items__input'>
+                    <input type={mostrarSenha} name="" id="" required/>
+                    <p className='form__items__placeholder'>Senha</p>
+                  </div>
+                  <button className='btnTransparente' onClick={verSenha} type="button">{senhaView}</button>
+                </div>
+                <Link className="form__link" to="/cadastro">
+                  <p>Não tem uma conta? Crie agora mesmo</p>
+                </Link>
+                <button className='form__btn__submit'>Entrar</button>
+              </form>
+              <div className="form__extra flex">
+                <div className="form__extra__hr">
+                  <hr className='form__hr'/>
+                  <p>Conheça mais</p>
+                </div>
+                <div className="form__redes-sociais">
+                  <a target="_blank" href="https://github.com/AurorinhaBoreal/SOC">
+                    <FaGithub className='form__redes-sociais__icon'/>
+                  </a>
                 </div>
               </div>
             </div>
-            <div className="form__items input-senha">
-              <RiLockPasswordLine className='icones'/>
-              <div className='form__items__input'>
-                <input type={mostrarSenha} name="" id="" required/>
-                <p className='form__items__placeholder'>Senha</p>
-              </div>
-              <button className='btnTransparente' onClick={verSenha} type="button">{senhaView}</button>
-            </div>
-            <Link className="form__link" to="/cadastro">
-              <p>Não tem uma conta? Crie agora mesmo</p>
-            </Link>
-            <button className='form__btn__submit'>Entrar</button>
-          </form>
-          <div className="form__extra flex">
-            <div className="form__extra__hr">
-              <hr className='form__hr'/>
-              <p>Conheça mais</p>
-            </div>
-            <div className="form__redes-sociais">
-              <a target="_blank" href="https://github.com/AurorinhaBoreal/SOC">
-                <FaGithub className='form__redes-sociais__icon'/>
-              </a>
+          </div>
+          <div className="form__direito">
+            <header className='form__direito__header'>
+              <img src={logo}/>
+            </header>
+            <div className='form__direito__conteudo'>
+              <h1 className='form__direito__texto'>Seja Bem Vindo <br />
+              ao SOC</h1>
             </div>
           </div>
         </div>
